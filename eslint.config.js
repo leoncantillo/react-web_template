7d4452652ed5320e52e84694fadbbfc59335c86f
Leon Cantillo <leonjob13@gmail.com>
2025-08-24 22:43:12 -0500

    [UPDATE] Eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,21 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import pluginReact from 'eslint-plugin-react';
 
 export default defineConfig([
   globalIgnores(['dist']),
   {
-    files: ['**/*.{js,jsx}'],
+    files: ['**/*.{js,mjs,cjs,jsx}'],
+    plugins: { js, react: pluginReact },
     extends: [
       js.configs.recommended,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
+      "js/recommended",
+      pluginReact.configs.flat.recommended,
     ],
     languageOptions: {
       ecmaVersion: 2020,
@@ -22,8 +26,16 @@ export default defineConfig([
         sourceType: 'module',
       },
     },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+
     rules: {
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'react/react-in-jsx-scope': 'off',
+      'semi': ['error', 'always'],
     },
   },
-])
+]);
